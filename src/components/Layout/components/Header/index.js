66c3f63f5259c21b9564f7cea66634c93200367d
“Your <you@example.com>
2024-01-21 21:04:14 +0700

    upload

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,20 @@
 import classNames from 'classnames/bind';
 import styles from './Header.mudule.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                </div>
+            </div>
         </header>
     );
 }
